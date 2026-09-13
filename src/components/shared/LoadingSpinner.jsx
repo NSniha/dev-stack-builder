0b2c967e6ad01d500e2eb3eeb2dd38f12a48df31
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-09-13 12:47:09 +0600

    Added loading spinner using daisyui

---
+++ src/components/shared/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="flex min-h-[260px] w-full items-center justify-center">
+      <span className="loading loading-spinner loading-lg text-[#e51676]"></span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
